c9d13e002dbd72377011df2c798def3fe94a2c1d
something <nnebp@users.noreply.github.com>
2018-12-06 04:08:46 -0500

    cmake working with SDL2image

---
+++ cmake-build-debug/CMakeFiles/C_Playgroud.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Playgroud.dir/TextTest/testText.c.o"
+  "C_Playgroud.pdb"
+  "C_Playgroud"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Playgroud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SDL2Test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL2Test.dir/TextTest/testText.c.o"
+  "CMakeFiles/SDL2Test.dir/TextTest/Texture.c.o"
+  "SDL2Test.pdb"
+  "SDL2Test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SDL2Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
